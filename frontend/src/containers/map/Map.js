43b9349cf989714c310de5342910f2e35f9ad157
I CHIEN LAI <isabel@Is-MacBook-Pro-2.local>
2019-06-13 00:10:59 +0800

    init_map

--- frontend/src/containers/map/Map.js
+++ frontend/src/containers/map/Map.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import GoogleMap from 'google-map-react';
 import './../../App.css';
 
 class Map extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: [25.021918, 121.535285],
+      zoom: 9
+    }
+  }
   render() {
     return (
       <div id="right_map">
-        Map
-      </div>
+        <GoogleMap center={this.state.center} zoom={this.state.zoom}>
 
+        </GoogleMap>
+      </div>
     );
   }
 }
