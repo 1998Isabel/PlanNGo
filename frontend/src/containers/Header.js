f570774123ca18713ebdea91c216d444d29e0dbc
a7532ariel <b05901176@ntu.edu.tw>
2019-06-14 14:04:40 +0800

    header

--- frontend/src/containers/Header.js
+++ frontend/src/containers/Header.js
@@ -1,15 +1,52 @@
 import React, { Component } from 'react';
 import './../App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        Header
-      </div>
 
-    );
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  bar: {
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
   }
+}));
+
+export default function Header() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="relative" className={classes.bar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            Header
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
-export default Header;
+
+
+
+
+
+// export default Header;
