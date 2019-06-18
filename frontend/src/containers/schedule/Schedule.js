5aea44d78551613c4e808bad62d1d1359fb0589d
I CHIEN LAI <isabel@Is-MacBook-Pro-2.local>
2019-06-18 17:08:00 +0800

    scroll_fix_scrollTo

--- frontend/src/containers/schedule/Schedule.js
+++ frontend/src/containers/schedule/Schedule.js
@@ -11,7 +11,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import { Element, scrollSpy, Events, Link } from 'react-scroll';
+import { Element, scrollSpy, Events, Link, scroller  } from 'react-scroll';
 
 class Schedule extends Component {
   constructor(props) {
@@ -26,6 +26,28 @@ class Schedule extends Component {
   handleChange = (event, newValue) => {
     this.setState({value: newValue})
   }
+  scrollToWithContainer(day) {
+    console.log(day)
+    let goToContainer = new Promise((resolve, reject) => {
+      Events.scrollEvent.register('end', () => {
+        resolve();
+        Events.scrollEvent.remove('end');
+      });
+      scroller.scrollTo("DayContainer", {
+        duration: 250,
+        delay: 0,
+        smooth: 'easeInOutQuart'
+      });
+    });
+
+    goToContainer.then(() =>  
+      scroller.scrollTo(day, {
+        duration: 800,
+        delay: 0,
+        smooth: 'easeInOutQuart',
+        containerId: "DayContainerElement"
+      }));
+  }
   render() {
     // const column = this.props.col["droppable-4"];
     // console.log(column);
@@ -33,9 +55,9 @@ class Schedule extends Component {
     let renderCols = this.props.columnOrder.map((colId, index) => {
       const column = this.props.col[colId];
       const items = column.items.map(itemId => this.props.items[itemId]);
-      const dayelement = "DAY" + (index + 2)
-      console.log("rol")
-      console.log(dayelement)
+      const dayelement = "DAY" + (index + 1)
+      // console.log("rol")
+      // console.log(dayelement)
       return (
         <Element key={column.id} name={dayelement} className="element">
           <DayBox key={column.id} column={column} items={items} index={index + 1} name={dayelement} handleDelete={this.handleDelete} />
@@ -57,12 +79,14 @@ class Schedule extends Component {
             {this.props.columnOrder.map((colId, index) => {
               const column = this.props.col[colId];
               const dayelement = "DAY" + (index + 1)
-              console.log("link")
-              console.log(dayelement)
+              // console.log("link")
+              // console.log(dayelement)
               return (
-                <Link key={column.id} to={dayelement} spy={true} smooth={true} duration={250} containerId="DayContainerElement">
-                  <Tab key={column.id} label={dayelement} value={index} style={{ padding: 4, width: 80, minWidth: 80 }} />
-                </Link>
+                <Tab key={column.id} label={dayelement} value={index} style={{ padding: 4, width: 80, minWidth: 80 }} onClick={() => this.scrollToWithContainer(dayelement)}>
+                  {/* <Link key={column.id} to={dayelement} spy={true} smooth={true} duration={250} containerId="DayContainerElement">
+                  </Link> */}
+                  {/* <a onClick={() => this.scrollTo()} ></a> */}
+                </Tab>
               );
             })}
           </Tabs>
