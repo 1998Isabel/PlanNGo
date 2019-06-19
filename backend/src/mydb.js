4eecb21c897af392c2fbc2e1d53ddc45230de8d2
Henry Laih <henrylaih41@yahoo.com>
2019-06-19 14:56:06 +0800

    add_backend_server

---
+++ backend/src/mydb.js
@@ -0,0 +1,64 @@
+const mydb = {
+    default: {
+        items: [
+            {
+                id: 1,
+                description:'this is test 1'
+            },
+            {
+                id: 2,
+                description:'this is test 2'
+            },
+            {
+                id: 3,
+                description:'this is test 3'
+            },
+            {
+                id: 4,
+                description:'this is test 4'
+            },
+            {
+                id: 5,
+                description:'this is test 5'
+            }
+        ],
+        schedule_columns: {
+            "droppable-4":{
+                id: "droppable-4",
+                items:['item4']
+            },
+            "droppable-5":{
+                id: "droppable-5",
+                items:['item5']
+            },
+            "droppable-6":{
+                id: "droppable-6",
+                items:['item2']
+            },
+            "droppable-7":{
+                id: "droppable-7",
+                items:['item3']
+            },
+        },
+        spots_columns: {
+            "droppable-1":{
+                id: "droppable-1",
+                items:[
+                    'item1'
+                ]
+            },
+            "droppable-2":{
+                id: "droppable-2",
+                items:[]
+            },
+            "droppable-3":{
+                id: "droppable-3",
+                items:[]
+            }
+        },
+        columnOrder: ["droppable-1","droppable-2", "droppable-3"],
+        dayOrder: ["droppable-4", "droppable-5", "droppable-6", "droppable-7"]
+    }
+};
+
+export default mydb;
---
+++ backend/src/resolvers/Query.js
@@ -0,0 +1,57 @@
+const Query = {
+  
+  items(parent, args, {db}, info){
+    if(!args.query){
+      return db.default.items
+    }
+
+    return db.default.items.filter(item => {
+      return item.id === args.query
+    })
+  },
+  
+  users(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.users
+    }
+
+    return db.users.filter(user => {
+      return user.name.toLowerCase().includes(args.query.toLowerCase())
+    })
+  },
+  posts(parent, args, { db }, info) {
+    if (!args.query) {
+      return db.posts
+    }
+
+    return db.posts.filter(post => {
+      const isTitleMatch = post.title
+        .toLowerCase()
+        .includes(args.query.toLowerCase())
+      const isBodyMatch = post.body
+        .toLowerCase()
+        .includes(args.query.toLowerCase())
+      return isTitleMatch || isBodyMatch
+    })
+  },
+  comments(parent, args, { db }, info) {
+    return db.comments
+  },
+  me() {
+    return {
+      id: '123098',
+      name: 'Mike',
+      email: 'mike@example.com'
+    }
+  },
+  post() {
+    return {
+      id: '092',
+      title: 'GraphQL 101',
+      body: '',
+      published: false
+    }
+  }
+}
+
+export { Query as default }
