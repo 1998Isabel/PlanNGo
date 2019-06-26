5797bcf9183b5377586c2415bb75c72ee399429b
a7532ariel <b05901176@ntu.edu.tw>
2019-06-26 20:26:04 +0800

    day

--- backend/src/resolvers/Day.js
+++ backend/src/resolvers/Day.js
@@ -1,15 +1,17 @@
+const User = require('../models/User')
+
 const Day = {
-    items(parent, args, { db }, info) {
-        // return db["Henry"].items.filter(post => {
-        //   return post.author === parent.id
-        // })
-        return parent.itemsid.map(itemid => {
-            for(var i = 0; i < db[args.id].items.length; i++) {
-                if (itemid === db[args.id].items[i].id) {
-                    return db[args.id].items[i];
+    async items(parent, args, { db }, info) {
+        const process = (result) => {
+            return parent.itemsid.map(itemid => {
+                for(var i = 0; i < result.items.length; i++) {
+                    if (itemid === result.items[i].id) {
+                        return result.items[i];
+                    }
                 }
-            }
-        })
+            })
+        }
+        return process(await User.findOne({usertoken: args.id}));
     },
 }
 
